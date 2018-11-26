d3576d42a35922786085471a78f1bcb4aef3976e
iklone <jamesthackway@hotmail.co.uk>
2018-11-26 01:09:52 +0000

    base for messenger-FB-Python communication

---
+++ automaid/config.py
@@ -0,0 +1,11 @@
+#Facebook Token
+ACCESS_TOKEN = "EAAEKFyAMe2wBAPT5rCPWZAKapbTUdUmpFBQZCpBCKkyxPZBt2kSef3266FbBdLZAN4ops5H2IYsvJgAXVhCYeyWbjDjTtwhPJ5Rr0Ggwfp2n8sBMRzHDivufmlo5EwY11Cdz7ZCfmIIllGJhDyvQtET3qI9ZBDh86xDKK0UJXtfAZDZD"
+VERIFY_TOKEN = "maid"
+
+#prints FB tokens
+def getFBTokens():
+    print("Access Token = " + ACCESS_TOKEN)
+    print("Verify Token = " + VERIFY_TOKEN)
+
+#curl -X GET "localhost:1337/webhook?hub.verify_token=maid&hub.challenge=CHALLENGE_ACCEPTED&hub.mode=subscribe"
+#curl -H "Content-Type: application/json" -X POST "localhost:1337/webhook" -d '{"object": "page", "entry": [{"messaging": [{"message": "TEST_MESSAGE"}]}]}'
---
+++ automaid/main.py
@@ -0,0 +1,52 @@
+import json
+import sys
+import tornado.ioloop
+import tornado.web
+
+from config import ACCESS_TOKEN, VERIFY_TOKEN
+
+class WebhookDriver(tornado.web.RequestHandler):
+    print("Webhook Driver Booted")
+
+    #GET - incoming
+    def get(self):
+        args = self.request.arguments
+        if args.get('hub.mode', [''])[0] == 'subscribe' and \
+        args.get('hub.verify_token', [''])[0] == VERIFY_TOKEN:
+            print("Verify Token Accepted")
+            return
+        print("Verify Token NOT Accepted, tried " + args.get('hub.verify_token', [''])[0])
+        self.set_status(403)
+        return
+
+    #POST - outgoing
+    def post(self):
+        body = json.loads(self.request.body)
+        for event in body.get('entry', []):
+            messaging = event['messaging']
+            for msg in messaging:
+                fb_message = msg.get('message', {})
+                print(fb_message)
+                #recipient_id = msg['sender']['id']
+                #print("Sender ID = " + recipient_id)
+                if 'text' in fb_message:
+                    #text message
+                    query = fb_message['text']
+                    print("Sender message = " + query)
+                elif 'attachments' in fb_message:
+                    #attachment message (image)
+                    print("Sender message is attachment")
+        print("Completed")
+
+def main():
+    #tornado
+    application = tornado.web.Application([(
+        r"/webhook", WebhookDriver),
+    ])
+
+    #start listening
+    application.listen(1337)
+    tornado.ioloop.IOLoop.current().start()
+
+if __name__ == "__main__":
+    main()
