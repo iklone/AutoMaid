1fa5addc79e5759baaa63f54225b2a7b9ad2382c
iklone <jamesthackway@hotmail.co.uk>
2018-12-17 17:14:05 +0000

    Initial trained AI

--- automaid/chatbot.py
+++ automaid/chatbot.py
@@ -4,9 +4,15 @@ class MaidChatDriver(object):
     def __init__(self, storage_adapter):
         self.chatbot = ChatBot(
             'AMAI',
+            trainer='chatterbot.trainers.ChatterBotCorpusTrainer',
             storage_adapter = 'chatterbot.storage.SQLStorageAdapter',
             database = './AMAI.sqlite3'
         )
+        '''
+        self.chatbot.train(
+            "chatterbot.corpus.english.conversations",
+            "chatterbot.corpus.english.greetings"
+        '''
         print "AMAI initialised"
 
     def get_response(self, query):
